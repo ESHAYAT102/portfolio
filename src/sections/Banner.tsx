49e158ea1289e69f5c110e0996c34e5f269ed062
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2026-06-14 02:20:13 +0600

    Update Banner.tsx

--- src/sections/Banner.tsx
+++ src/sections/Banner.tsx
@@ -223,8 +223,8 @@ export default function Banner() {
                   years old
                 </span>
               </button>{" "}
-              designer, and I love to code (based in Bangladesh). I've always
-              been into computers. I was fascinated by the technology. Like{" "}
+              designer, and I love to code. I'm based in Bangladesh. I've always
+              been into computers. I was fascinated by the technology, like{" "}
               <button
                 className={tooltipButtonClass}
                 type="button"
