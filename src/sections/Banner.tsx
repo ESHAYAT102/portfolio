784fe62245d8fc7d74bd43f9f38669c1e63f8246
Eshayat Al-Wasiu <eshayat.wasiu@gmail.com>
2026-07-02 02:12:56 +0600

    positioning fixes

--- src/sections/Banner.tsx
+++ src/sections/Banner.tsx
@@ -132,7 +132,7 @@ export default function Banner() {
     <div>
       <div className="w-screen min-[1000px]:w-full flex justify-center align-middle items-center">
         <div className="mx-0 min-[1000px]:mx-38 w-full border-b border-l-0 min-[1000px]:border-l border-r-0 min-[1000px]:border-r border-stone-600/60">
-          <div className="px-4 min-[1000px]:px-20 py-20">
+          <div className="px-4 min-[1000px]:px-20 pt-10 pb-20 min-[760px]:py-20">
             <p>Designer & Frontend Developer</p>
             <h1 className="text-3xl font-semibold mt-10">
               Hi there!
